6bc86267ba3210620b7e4c6e83ec4602a71ae1a0
Aura Development <146517988+auradevelopment5m@users.noreply.github.com>
2025-04-27 09:34:58 +0300

    Update fxmanifest.lua
    - Updated version
    - Updated Author name
    - Updated Description

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
 games { 'gta5' }
 lua54 'yes'
-author 'ZDevelopment'
-description 'Trash Searching Script By ZDevelopment'
-version '1.0.0'
+name 'aura-dumpsterdiving'
+author 'Aura Development'
+description 'Trash Searching Script By Aura Development'
+version '2.0'
 
 server_scripts {
     'config.lua',
